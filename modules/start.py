30c766c82ddec47ea158e4e2cd01aa189ae8f70a
casper9 <50382728+casper9@users.noreply.github.com>
2023-07-13 08:57:36 +0800

    Update start.py

--- modules/start.py
+++ modules/start.py
@@ -40,9 +40,7 @@ def start(d: Message):
         '/bath_do - batch test accounts\n' \
         '/add_vps - create droplets\n' \
         '/sett_vps - manage droplets\n' \
-        ' \n' \
-        '<b>Dev: @yha_bot</b>\n' \
-        '<b>Support: @fightertunnell</b>'
+        ' \n'
     bot.send_message(
         text=t,
         chat_id=d.from_user.id,
